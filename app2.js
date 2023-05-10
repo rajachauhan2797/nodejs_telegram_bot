7db995086187751d0482e69a4f41b800bcafee99
raja <raja.chauhan@shipyaari.com>
2023-05-10 20:08:47 +0530

    testing done working

---
+++ app2.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const expressApp = express();
+const axios = require("axios");
+const path = require("path");
+const port = process.env.PORT || 3000;
+expressApp.use(express.static("static"));
+expressApp.use(express.json());
+require("dotenv").config();
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.bot_token);
+
+expressApp.get("/", (req, res) => {
+  console.log("dnfsdjk");
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+expressApp.listen(port,()=>{
+    console.log("dnfsdjk");
+});
+
+bot.launch();
+
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it",
+    {}
+  );
+});
+
+bot.command("ethereum", (ctx) => {
+  var rate;
+  console.log(ctx.from);
+  axios
+    .get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`
+    )
+    .then((response) => {
+      console.log(response.data);
+      rate = response.data.ethereum;
+      const message = `Hello, today the ethereum price is ${rate.usd}USD`;
+      bot.telegram.sendMessage(ctx.chat.id, message, {});
+    });
+});
